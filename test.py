79d2bdb42de6281e4be77c9d4bee067ce62bed14
Martin Page <wookoouk@gmail.com>
2016-04-21 16:47:27 +0100

    example

---
+++ test.py
@@ -0,0 +1,19 @@
+from dronekit import connect
+
+# Connect to the Vehicle.
+print "Connecting to vehicle on: 'tcp:127.0.0.1:5760'"
+vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
+
+# Get some vehicle attributes (state)
+print "Get some vehicle attribute values:"
+print " GPS: %s" % vehicle.gps_0
+print " Battery: %s" % vehicle.battery
+print " Last Heartbeat: %s" % vehicle.last_heartbeat
+print " Is Armable?: %s" % vehicle.is_armable
+print " System status: %s" % vehicle.system_status.state
+print " Mode: %s" % vehicle.mode.name  # settable
+
+# Close vehicle object before exiting script
+vehicle.close()
+
+print("Completed")
